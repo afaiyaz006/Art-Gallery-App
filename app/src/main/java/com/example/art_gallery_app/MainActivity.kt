08b77b0588f29a5536c8ca93611e2fb493ed22e0
afaiyaz006 <69722755+afaiyaz006@users.noreply.github.com>
2025-05-01 07:01:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/art_gallery_app/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.art_gallery_app
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.art_gallery_app.ui.theme.ArtGalleryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtGalleryAppTheme {
+                Surface(){
+                    ArtGalleryAppLayOut(
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ArtGalleryAppLayOut(modifier: Modifier= Modifier){
+
+        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Row() {
+                ImageViewer(modifier.padding(32.dp))
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(){
+                ArtWorkInfo(modifier)
+            }
+            Row(){
+                Button(onClick = {  /*nothing*/}) {
+                    Text("Filled")
+                }
+                Button(onClick = {  /*nothing*/}) {
+                    Text("Filled")
+                }
+            }
+        }
+
+
+}
+@Composable
+fun ImageViewer(modifier: Modifier){
+    var imageValue = painterResource(R.drawable.ic_launcher_background)
+    Image(
+        modifier=modifier.wrapContentSize(Alignment.Center).height(350.dp).width(250.dp),
+        painter = imageValue,
+        contentScale = ContentScale.FillHeight,
+        contentDescription = null
+    )
+}
+@Composable
+fun ArtWorkInfo(modifier: Modifier){
+    Column(){
+        Text(
+            text="Artwork title",
+
+        )
+        Text(
+            text="Artwork artist"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtGalleryAppTheme {
+        Surface(modifier = Modifier.fillMaxSize()){
+            ArtGalleryAppLayOut()
+        }
+    }
+}
